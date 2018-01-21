483ae5f52a4d1d402e24d6a473d16c891d01d14a
ChenxiYuan <yuanchenxi95@gmail.com>
2018-01-21 15:56:53 -0500

    auto push

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ if __name__ == '__main__':
     # for pcap in pcap_list:
     #     print(pcap)
     mac_log_dict = dict()
-    for dirname, dirnames, filenames in os.walk('/home/traffic/unctrl'):
+    for dir_name, dir_names, file_names in os.walk('/home/traffic/unctrl'):
         # print path to all subdirectories first.
-        mac_log_dict[dirname] = filenames
+        files_with_log_extension = []
+        for file in file_names:
+            if file.endswith('.log'):
+                files_with_log_extension.append(file)
+        mac_log_dict[dir_name] = files_with_log_extension
 
     print(mac_log_dict)
 
