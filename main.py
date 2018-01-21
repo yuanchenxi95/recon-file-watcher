4be489b65f592b720ce220e369264909ef6da51d
ChenxiYuan <yuanchenxi95@gmail.com>
2018-01-21 15:54:56 -0500

    auto push

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ if __name__ == '__main__':
     # pcap_list = process_pcap('/Users/ChenxiYuan/Project/ReconFileWatcher/2017-12-08_19.00.02_192.168.10.131.pcap')
     # for pcap in pcap_list:
     #     print(pcap)
-    mac_or_ip_addresses = []
+    mac_log_dict = dict()
     for dirname, dirnames, filenames in os.walk('/home/traffic/unctrl'):
         # print path to all subdirectories first.
-        mac_or_ip_addresses.append(dirname)
-        print(filenames)
-        print()
+        mac_log_dict[dirname] = filenames
 
-    print(mac_or_ip_addresses)
+    print(mac_log_dict)
 
