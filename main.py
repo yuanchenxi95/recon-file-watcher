3b3e38f425e9186a6e6a9e7b9ca4d348ca48475a
ChenxiYuan <yuanchenxi95@gmail.com>
2018-01-24 09:53:40 -0500

    auto push

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from pcap_process import process_pcap
 from http_log_process import process_http_log
 import os
-import requests
 import json
 
 if __name__ == '__main__':
@@ -23,8 +22,5 @@ if __name__ == '__main__':
             # print(folder)
             ip_processed_data_dict[log_file] = process_http_log(folder + '/' + log_file)
     print(ip_processed_data_dict)
-    r = requests.post("https://api.myjson.com/bins", ip_processed_data_dict)
-    print(r.status_code)
-    print(r.headers)
-    print(r.content)
-
+    with open('http.log', 'w') as f:
+        f.write(ip_processed_data_dict)
