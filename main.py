d4d59e49ae7f487c691f3d93687ddb32036f3fac
ChenxiYuan <yuanchenxi95@gmail.com>
2018-01-27 23:52:10 -0500

    auto push

--- main.py
+++ main.py
@@ -5,25 +5,21 @@ import json
 import requests
 
 if __name__ == '__main__':
-    # pcap_list = process_pcap('/Users/ChenxiYuan/Project/ReconFileWatcher/2017-12-08_19.00.02_192.168.10.131.pcap')
-    # for pcap in pcap_list:
-    #     print(pcap)
     mac_log_dict = dict()
     for dir_name, dir_names, file_names in os.walk('/home/traffic/unctrl'):
         # print path to all subdirectories first.
-        files_with_log_extension = []
+        files_with_pcap_extension = []
         for file in file_names:
-            if file.endswith('.log'):
-                files_with_log_extension.append(file)
-        mac_log_dict[dir_name] = files_with_log_extension
+            if file.endswith('.pcap'):
+                files_with_pcap_extension.append(file)
+        mac_log_dict[dir_name] = files_with_pcap_extension
 
-    ip_processed_data_dict = dict()
-    for folder in mac_log_dict:
-        for log_file in mac_log_dict[folder]:
-            # print(folder + '/' + log_file)
-            ip_processed_data_dict[folder] = process_http_log(folder + '/' + log_file)
-    # requests.post('http://54.193.126.147:3000/', ip_processed_data_dict)
-
-    # print(ip_processed_data_dict)
-    with open('http.json', 'w') as f:
-        f.write(str(ip_processed_data_dict))
+    print(mac_log_dict)
+    # ip_processed_data_dict = dict()
+    # for folder in mac_log_dict:
+    #     for log_file in mac_log_dict[folder]:
+    #         ip_processed_data_dict[folder] = process_http_log(folder + '/' + log_file)
+    #
+    # # print(ip_processed_data_dict)
+    # with open('http.json', 'w') as f:
+    #     f.write(str(ip_processed_data_dict))
