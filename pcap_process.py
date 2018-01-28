94765e84531d3f11c24c3d2b8a6163cbe10be824
ChenxiYuan <yuanchenxi95@gmail.com>
2018-01-28 00:51:21 -0500

    auto push

--- pcap_process.py
+++ pcap_process.py
@@ -55,54 +55,45 @@ def process_http_requests(pcap):
     pcap_data_list = []
     for timestamp, buf in pcap:
 
-        # Unpack the Ethernet frame (mac src/dst, ethertype)
-        eth = dpkt.ethernet.Ethernet(buf)
+        try:
+            # Unpack the Ethernet frame (mac src/dst, ethertype)
+            eth = dpkt.ethernet.Ethernet(buf)
 
-        # Make sure the Ethernet data contains an IP packet
-        if not isinstance(eth.data, dpkt.ip.IP):
-            continue
-
-        # Now grab the data within the Ethernet frame (the IP packet)
-        ip = eth.data
-
-        # Check for TCP in the transport layer
-        if isinstance(ip.data, dpkt.tcp.TCP):
-
-            # Set the TCP data
-            tcp = ip.data
-
-            # Now see if we can parse the contents as a HTTP request
-            try:
-                request = dpkt.http.Request(tcp.data)
-            except (dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
+            # Make sure the Ethernet data contains an IP packet
+            if not isinstance(eth.data, dpkt.ip.IP):
                 continue
 
-            # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
-            do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
-            more_fragments = bool(ip.off & dpkt.ip.IP_MF)
-            fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+            # Now grab the data within the Ethernet frame (the IP packet)
+            ip = eth.data
 
-            # Print out the info
-            # print('Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
-            # print('Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type)
-            # print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)' %
-            #       (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
-            # print('HTTP request: %s\n' % repr(request))
-            # print('Headers: %s\n' % request.headers)
+            # Check for TCP in the transport layer
+            if isinstance(ip.data, dpkt.tcp.TCP):
 
-            # Check for Header spanning acrossed TCP segments
-            # if not tcp.data.endswith(b'\r\n'):
-                # print('\nHEADER TRUNCATED! Reassemble TCP segments!\n')
+                # Set the TCP data
+                tcp = ip.data
 
-            pcap_data_object = PcapDataClass(eth_src_mac=mac_addr(eth.src),
-                                             eth_dst_mac=mac_addr(eth.dst),
-                                             src_ip=inet_to_str(ip.src),
-                                             dst_ip=inet_to_str(ip.dst),
-                                             header_host=request.headers['host'],
-                                             http_method=request.method,
-                                             time_stamp=timestamp
-                                             )
-            pcap_data_list.append(pcap_data_object)
+                # Now see if we can parse the contents as a HTTP request
+                try:
+                    request = dpkt.http.Request(tcp.data)
+                except (dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
+                    continue
+
+                # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
+                do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
+                more_fragments = bool(ip.off & dpkt.ip.IP_MF)
+                fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+
+                pcap_data_object = PcapDataClass(eth_src_mac=mac_addr(eth.src),
+                                                 eth_dst_mac=mac_addr(eth.dst),
+                                                 src_ip=inet_to_str(ip.src),
+                                                 dst_ip=inet_to_str(ip.dst),
+                                                 header_host=request.headers['host'],
+                                                 http_method=request.method,
+                                                 time_stamp=timestamp
+                                                 )
+                pcap_data_list.append(pcap_data_object)
+        except:
+            continue
     return pcap_data_list
 
 
