17590f9aa7eb910497fa27f5a07d5dc5630673bc
ChenxiYuan <yuanchenxi95@gmail.com>
2018-02-03 15:23:37 -0500

    auto push

--- http_log_watcher/log_watcher.py
+++ http_log_watcher/log_watcher.py
@@ -1,6 +1,6 @@
 from .http_log_process import process_http_log
 import os
-import json
+import requests
 
 
 def get_logfile_list(ctl_name):
@@ -15,20 +15,25 @@ def get_logfile_list(ctl_name):
     return mac_log_dict
 
 
+def get_log_file_uri(mac_address, date_string):
+    api_uri = 'http://54.193.126.147:3000/api/networkData/logFileData/'
+    return api_uri + mac_address + '/' + date_string
+
+
 def run_processing_log_files_of_all_directories():
     mac_log = get_logfile_list('/home/traffic/unctrl')
     mac_http_dict = dict()
+    api_uri = "http://54.193.126.147:3000/api/networkData/logFileData/"
 
     for dir_name, log_name_list in mac_log.items():
         http_data_dict = dict()
-        mac_http_dict[dir_name[21:]] = http_data_dict
+        mac_address = dir_name[21:]
+        mac_http_dict[mac_address] = http_data_dict
         for log_name in log_name_list:
             k = dir_name + '/' + log_name
-            http_data_dict[log_name] = process_http_log(k)
+            http_log_list = process_http_log(k)
+            http_data_dict[log_name] = http_log_list
             if len(http_data_dict[log_name]) == 0:
                 continue
-    with open('result.json', 'w') as fp:
-        json.dump(mac_http_dict, fp)
-
-        # r = requests.post("http://54.193.126.147:3000/api/networkData/todayData", json=mac_http_dict)
-        # print(r.content)
+            r = requests.post(get_log_file_uri(mac_address, log_name), json=http_log_list)
+            print(r.content)
