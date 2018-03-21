d76f19b3b2644e51b7f51729f27af092a07b4dff
ChenxiYuan <yuanchenxi95@gmail.com>
2018-03-21 00:04:06 -0400

    auto push

--- device/updateDeviceList.py
+++ device/updateDeviceList.py
@@ -2,4 +2,5 @@ if __name__ == '__main__':
     fname = '/home/traffic/devices.txt'
     with open(fname) as f:
         content = f.readlines()
-        print(content)
+    content = [x.strip() for x in content]
+    print(content)
