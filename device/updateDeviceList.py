e9cfca7dcbba80bf30400bc63ac05c6bc3801633
ChenxiYuan <yuanchenxi95@gmail.com>
2018-03-21 00:08:43 -0400

    auto push

--- device/updateDeviceList.py
+++ device/updateDeviceList.py
@@ -1,6 +1,17 @@
+import requests
+
+HOST_URL = 'https://moniotr-smart-router-server.herokuapp.com/'
+
 if __name__ == '__main__':
     fname = '/home/traffic/devices.txt'
     with open(fname) as f:
         content = f.readlines()
     content = [x.strip() for x in content]
-    print(content)
+    deviceList = []
+    for row in content:
+        deviceList.append({
+            'mac_address': row[0],
+            'alias': row[1]
+        })
+    requests.post(HOST_URL + 'api/device/updateDeviceList', data=deviceList)
+
